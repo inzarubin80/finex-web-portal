0ee1456850dc29e8508e4ee09b0d1f80360fd940
inzarubin80@gmail.com <inzarubin80@gmail.com>
2021-06-06 15:07:38 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { API_URL, username, password } from './Constants'
+
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import axios from 'axios'
+
+
+
+const auth = {auth: {username, password}} 
+
+
+
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+
+
+const Test =  () => {
+
+  
+  axios.post(`${API_URL}/?typerequest=sendConformationCode`,{},{s:1, d2:2})
+  axios.get(`${API_URL}/?typerequest=sendConformationCode`)
+  
+  
+ 
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit ggggggg<code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
+      <Button variant="contained" color="primary" onClick={Test}>
+        ТЕСТ111
+      </Button>
+
+
     </div>
   );
 }
---
+++ src/Constants.js
@@ -0,0 +1,4 @@
+  
+export const API_URL = 'http://localhost/UPP_ZARUBIN/hs/items/'
+export const username = "WebServiceAccount" 
+export const password = "123456789"
